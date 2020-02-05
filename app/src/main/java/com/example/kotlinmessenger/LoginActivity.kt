e2ebee87aae4b00cf39d6c327c3756d23d94dd6b
Khaled Hamed <khaled.hamedt@gmail.com>
2020-02-05 22:35:07 +0200

    Firebase Signup

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinmessenger
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -9,5 +11,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text;
+            val password = password_edittext_login.text;
+
+            Log.d("Login", "Attempt login with email/pw: $email/pw")
+        }
+
+        back_to_register_textview.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -4,29 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val email = username_edittext_register.text.toString()
+            val email = email_edittext_register.text.toString()
             val password = password_edittext_register.text.toString()
 
-            Log.d("MainActivity", "Email: $email")
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+                Log.d("MainActivity", "Email: $email")
             Log.d("MainActivity", "Password: $password")
+
+            auth = FirebaseAuth.getInstance()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (!task.isSuccessful) return@addOnCompleteListener
+
+                    // else if successful
+                    Log.d("Main", "Succussfully created user with uid: ${task.result?.user?.uid}")
+                }
+
+            already_have_account_text_view.setOnClickListener {
+                Log.d("MainActivity", "Login Activity")
+
+                // Launch login activity
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
-
-        already_have_account_text_view.setOnClickListener {
-            Log.d("MainActivity", "Login Activity")
-
-            // Launch login activity
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
